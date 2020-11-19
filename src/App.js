55db708753c1ccc70443299cac2ff3b430e0309d
kai.guo <kai.guo@langogo.ai>
2020-11-19 17:34:18 +0800

    result preview

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import { useState } from 'react'
 import SimpleEditor from './SimpleEditor'
 
-function onChange(doc) {
-  console.log('app doc,onchange', doc)
+function ResultDiv(props) {
+  return <pre>{JSON.stringify(props.value, null, 2)}</pre>
 }
 
 function App() {
+  const [value, setValue] = useState('')
+  function onChange(doc) {
+    console.log('app doc,onchange', doc)
+    setValue(doc)
+  }
   return (
-    <div
-      className="App"
-      style={{
-        border: '1px dashed',
-      }}
-    >
-      <SimpleEditor value="这是初始数据" onChange={onChange}></SimpleEditor>
+    <div>
+      <SimpleEditor value={value} onChange={onChange}></SimpleEditor>
+      <ResultDiv value={value}></ResultDiv>
     </div>
   )
 }
